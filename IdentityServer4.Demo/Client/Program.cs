0f7016ab08632d312bfd3ab605f1e8f979a54831
DESKTOP\THINH <trangiathinh190697@gmail.com>
2023-02-25 17:31:22 +0700

    init projects

---
+++ IdentityServer4.Demo/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using IdentityModel.Client;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "api1"
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    return;
+}
+
+Console.WriteLine(tokenResponse.Json);
---
+++ IdentityServer4.Demo/Demo.Api/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Api.Controllers
+{
+    [Route("api/identity")]
+    [ApiController]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        public IdentityController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var claims = User.Claims.Select(x => new { x.Type, x.Value });
+            return await Task.FromResult(Ok(claims));
+        }
+    }
+}
